7b6b6f30184a7bf2856b943542c64ec534bc301e
btwiusegentoo <66811008+btwiusegentoo@users.noreply.github.com>
2020-07-13 07:44:17 +0900

    :wrench: Change border width.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -16,7 +16,7 @@ myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
-myBorderWidth   = 3
+myBorderWidth   = 2
 myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 myNormalBorderColor  = "#fefefe"
 myFocusedBorderColor = "#f07178"
