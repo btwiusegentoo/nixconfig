7eeb751dfc18b5a4bb67dcca3317913559b221d2
btwiusegentoo <66811008+btwiusegentoo@users.noreply.github.com>
2020-10-28 17:38:10 +0900

    Upgrading server to 20.09

--- machines/mainserver/hardware-configuration.nix
+++ machines/mainserver/hardware-configuration.nix
@@ -15,15 +15,20 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.enableCryptodisk = true;
 
-  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/92eea9d5-adfb-44a7-b074-edfcc021fe19";
+  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/29e93967-944e-4306-adb2-8d2441ca5ed5";
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/05d34d57-5225-4e81-8ddc-c6a16fc88b3d";
+    { device = "/dev/disk/by-uuid/4979bbc1-28e8-4505-9a3e-2d2fc0f41fa3";
       fsType = "xfs";
     };
 
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/f5979f75-d494-49ba-a149-82a612dcf543";
+      fsType = "ext2";
+    };
+
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/6ff4e3d6-fa06-4faa-974a-99d3e7608527"; }
+    [ { device = "/dev/disk/by-uuid/8bb10870-d71d-484b-a861-d702a2f55484"; }
     ];
 
   nix.maxJobs = lib.mkDefault 2;
