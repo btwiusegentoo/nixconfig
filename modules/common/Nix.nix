1dd69c70c497a7cff4edda614f75e30e0dec8c58
btwiusegentoo <66811008+btwiusegentoo@users.noreply.github.com>
2020-10-29 17:49:27 +0900

    remove distributed builds

--- modules/common/Nix.nix
+++ modules/common/Nix.nix
@@ -2,37 +2,9 @@
 
 {
     package = pkgs.nixUnstable;
-    distributedBuilds = true;
 
     extraOptions = ''
         experimental-features = nix-command flakes
         builders-use-substitutes = true
     '';
-
-    buildMachines = [
-    {
-        hostName = "desktop1";
-        system = "x86_64-linux";
-        maxJobs = 8;
-        speedFactor = 3;
-        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-        mandatoryFeatures = [ ];
-    }
-    {
-        hostName = "server1";
-        system = "x86_64-linux";
-        maxJobs = 2;
-        speedFactor = 0;
-        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-        mandatoryFeatures = [ ];
-    }
-    {
-        hostName = "laptop1";
-        system = "x86_64-linux";
-        maxJobs = 4;
-        speedFactor = 2;
-        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-        mandatoryFeatures = [ ];
-    }
-    ];
 }
