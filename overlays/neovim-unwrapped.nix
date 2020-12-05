bace27f983cc34fd6878404c484c6640c94ef2d8
btwiusegentoo <66811008+btwiusegentoo@users.noreply.github.com>
2020-12-05 15:35:42 +0900

    maybe fixed neovim master

--- overlays/neovim-unwrapped.nix
+++ overlays/neovim-unwrapped.nix
@@ -4,8 +4,9 @@ final: prev:
   neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (old: rec {
     ## use neovim nightly
     version = "master";
-    src = builtins.fetchGit {
-      url = "https://github.com/neovim/neovim";
+    src = builtins.fetchFromGitHub {
+      owner = "neovim";
+      repo = "neovim";
       rev = "8fb786e415d1c3538452885455b2268d13f640a6";
       sha256 = "1smq0y7r3s2qqsi11vr9jb5xmch8p9krfxmx9rrmfg7qn38awzkg";
     };
